ee058717f8b4903bfdbb9bb6aa11f1f62d2441d6
iqtedar-710 <iqtedarahmed70@gmail.com>
2026-05-15 12:43:14 +0500

    fixed indentation

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def download_video():
 
     video_url = data.get("url")
     download_type = data.get("type", "mp4")
-    quality = data.get("quality", "best")
+    quality = data.get("quality", "720")
 
     if not video_url:
         return jsonify({
@@ -33,49 +33,50 @@ def download_video():
         f"{unique_id}.%(ext)s"
     )
 
-    if download_type == "mp3":
-
-        ydl_opts = {
-            'format': 'bestaudio/best',
-            'outtmpl': output_template,
-            'cookiefile': 'cookies.txt',
-            'noplaylist': True,
-            'quiet': True,
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': quality,
-            }],
-        }
-
-    else:
-
-       format_map = {
-    "144": "bestvideo[height<=144]+bestaudio/best/best",
-    "240": "bestvideo[height<=240]+bestaudio/best/best",
-    "360": "bestvideo[height<=360]+bestaudio/best/best",
-    "480": "bestvideo[height<=480]+bestaudio/best/best",
-    "720": "bestvideo[height<=720]+bestaudio/best/best",
-    "1080": "bestvideo[height<=1080]+bestaudio/best/best",
-    "1440": "bestvideo[height<=1440]+bestaudio/best/best",
-    "2160": "bestvideo[height<=2160]+bestaudio/best/best",
-    "4320": "bestvideo[height<=4320]+bestaudio/best/best"
-}
-        selected_format = format_map.get(
-            quality,
-            "best"
-        )
-
-        ydl_opts = {
-            'format': selected_format,
-            'merge_output_format': 'mp4',
-            'outtmpl': output_template,
-            'cookiefile': 'cookies.txt',
-            'noplaylist': True,
-            'quiet': True,
-        }
-
     try:
+
+        if download_type == "mp3":
+
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'outtmpl': output_template,
+                'cookiefile': 'cookies.txt',
+                'noplaylist': True,
+                'quiet': True,
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': quality,
+                }],
+            }
+
+        else:
+
+            format_map = {
+                "144": "bestvideo[height<=144]+bestaudio/best/best",
+                "240": "bestvideo[height<=240]+bestaudio/best/best",
+                "360": "bestvideo[height<=360]+bestaudio/best/best",
+                "480": "bestvideo[height<=480]+bestaudio/best/best",
+                "720": "bestvideo[height<=720]+bestaudio/best/best",
+                "1080": "bestvideo[height<=1080]+bestaudio/best/best",
+                "1440": "bestvideo[height<=1440]+bestaudio/best/best",
+                "2160": "bestvideo[height<=2160]+bestaudio/best/best",
+                "4320": "bestvideo[height<=4320]+bestaudio/best/best"
+            }
+
+            selected_format = format_map.get(
+                quality,
+                "best"
+            )
+
+            ydl_opts = {
+                'format': selected_format,
+                'merge_output_format': 'mp4',
+                'outtmpl': output_template,
+                'cookiefile': 'cookies.txt',
+                'noplaylist': True,
+                'quiet': True,
+            }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
 
